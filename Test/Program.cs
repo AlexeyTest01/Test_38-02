aa037c453a8128d13e7f647e9c66f4404fd3c143
RamilBUGmanoff <ramilbagmanov21337@gmail.com>
2021-02-26 16:22:32 +0300

    хахахахахахахахаххахахахахха взломал комп) 0_0

--- Test/Program.cs
+++ Test/Program.cs
@@ -4,10 +4,18 @@ namespace Test
 {
     class Program
     {
+        
         static void Main(string[] args)
-        {
+        {   
+            int n = 0;
             Console.WriteLine("Hello World!");
             Console.WriteLine("Korshunov");
+            Console.WriteLine("ЭТОТ КОД ЗАПРИВАЧЕН");
+            for(int i = 0; i < n; n++)
+            {
+                Console.WriteLine("Происходит взлом компутера   " + n + "%");
+
+            }
         }
     }
 }
